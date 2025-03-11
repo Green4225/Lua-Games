aea518f4180d14ae343ecfbc8ee56afc5c6f3770
Green4225 <GreenOverflow1@gmail.com>
2025-03-11 15:54:40 +0000

    Font & Sound

--- Games/NumberGuess/main.lua
+++ Games/NumberGuess/main.lua
@@ -1,64 +1,78 @@
-local title = "Guess my Number"
-love.window.setTitle(title)
+function Restart()
+    local title = "Guess my Number"
+    love.window.setTitle(title)
 
-math.randomseed(os.time())
-local targetNumber = math.random(1, 1000)
+    math.randomseed(os.time())
+    TargetNumber = math.random(1, 1000)
 
-local playerGuess = ""
-local message = ""
+    PlayerGuess = ""
+    Message = ""
 
-local gameOver = false
+    GameOver = false
+
+    Time = 0
+end
+
+Restart()
+
+function love.update(dt)
+    Time = Time + dt
+end
 
 function love.textinput(input)
     if tonumber(input) then
-        playerGuess = playerGuess .. input
+        PlayerGuess = PlayerGuess .. input
     end
 end
 
 function love.keypressed(keyInput)
-    if keyInput == "return" and not gameOver then
+    if keyInput == "return" and not GameOver then
         CheckGuess()
     elseif keyInput == "backspace" then
-        playerGuess = playerGuess:sub(1, -2)
-    elseif keyInput == "return" and gameOver == true then
-        love.load()
+        PlayerGuess = PlayerGuess:sub(1, -2)
+    elseif keyInput == "return" and GameOver == true then
+        Restart()
     end
 end
 
 function CheckGuess()
-    local playerGuessAsNumber = tonumber(playerGuess)
+    local playerGuessAsNumber = tonumber(PlayerGuess)
     if playerGuessAsNumber then
-        if playerGuessAsNumber < targetNumber then
-            message = "Too low, try again!"
-        elseif playerGuessAsNumber > targetNumber then
-            message = "Too high, try again!"
+        if playerGuessAsNumber < TargetNumber then
+            Message = "Too low, try again!"
+        elseif playerGuessAsNumber > TargetNumber then
+            Message = "Too high, try again!"
         else
-            message = "You guessed it. You win!"
-            gameOver = true
+            Message = "You guessed it. You win!"
+            GameOver = true
         end
     end
-    if not gameOver then
-        playerGuess = ""
+    if not GameOver then
+        PlayerGuess = ""
     end
 end
 
 function love.load()
     Back = love.graphics.newImage("assets/pic.png")
 
+    local gameFont = love.graphics.newFont("assets/SuperShape-PV9qE.ttf", 24)
+    love.graphics.setFont(gameFont)
+
     local music = love.audio.newSource("assets/space-music.ogg", "stream")
     music:play()
-
 end
 
 function love.draw()
     love.graphics.draw(Back, 0, 0, 0, love.graphics.getWidth() / 1920, love.graphics.getHeight() / 1080)
 
-    love.graphics.printf("Guess my Number!", 0, 150, love.graphics.getWidth(), "center")
-    love.graphics.printf("Your Guess: " .. playerGuess, 0, 171, love.graphics.getWidth(), "center")
-    love.graphics.printf(message, 0, 192, love.graphics.getWidth(), "center")
+    TextBob = math.sin(Time * 2) * 5
 
-    if gameOver then
-        love.graphics.printf("Press enter to play again!", 0, 213, love.graphics.getWidth(), "center")
+    love.graphics.printf("Guess my Number!", 0, 150 + TextBob, love.graphics.getWidth(), "center")
+    love.graphics.printf("Your Guess: " .. PlayerGuess, 0, 192 + TextBob, love.graphics.getWidth(), "center")
+    love.graphics.printf(Message, 0, 234 + TextBob, love.graphics.getWidth(), "center")
+
+    if GameOver then
+        love.graphics.printf("Press enter to play again!", 0, 276 + TextBob, love.graphics.getWidth(), "center")
     end
 
 end
