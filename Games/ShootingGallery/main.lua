52305d2d85ba222a66f8be7eabbc6c2e1dfb63d1
Green4225 <GreenOverflow1@gmail.com>
2025-03-12 13:53:43 +0000

    Target and Score

--- Games/ShootingGallery/main.lua
+++ Games/ShootingGallery/main.lua
@@ -1,11 +1,37 @@
 function love.load()
- Number = 0
+
+    local title = "Shooting Gallery"
+    love.window.setTitle(title)
+
+    TargetTable = {}
+    TargetTable.x = 300
+    TargetTable.y = 300
+    TargetTable.radius = 50
+
+    Refous = {}
+    Refous.r = 168/255
+    Refous.g = 28/255
+    Refous.b = 7/255
+
+    Score = 0
+
+    Timer = 0
+
+    GameFont = love.graphics.newFont(40)
+
 end
 
 function love.update(dt)
- Number = Number + 1
+
 end
 
 function love.draw()
-love.graphics.print(Number)
+
+    love.graphics.setColor(Refous.r, Refous.g, Refous.b)
+    love.graphics.circle("fill", TargetTable.x, TargetTable.y, TargetTable.radius)
+
+    love.graphics.setColor(1, 1, 1)
+    love.graphics.setFont(GameFont)
+    love.graphics.print(Score)
+
 end
