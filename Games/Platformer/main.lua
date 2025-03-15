c6c11a6147acc64729782fd134ccbec2fcaec2fd
Green4225 <GreenOverflow1@gmail.com>
2025-03-15 22:01:33 +0000

    Very Basic Breezefield

--- Games/Platformer/main.lua
+++ Games/Platformer/main.lua
@@ -1,16 +1,23 @@
 function love.load()
 
     Breezefield = require "libraries/breezefield"
-    World = Breezefield.newWorld(0, 0)
+    World = Breezefield.newWorld(0, 90.81)
 
     Player = Breezefield.Collider.new(World, "Rectangle", 360, 100, 80, 80)
+    Platform = Breezefield.Collider.new(World, "Rectangle", love.graphics.getWidth() / 2, love.graphics.getHeight() / 2,
+    300, 100)
+    Platform:setType("static")
 
 end
 
 function love.update(dt)
 
+    World:update(dt)
+
 end
 
 function love.draw()
+
+    World:draw()
 
 end
